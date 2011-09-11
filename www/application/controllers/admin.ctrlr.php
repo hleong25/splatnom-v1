a78b094594dbc94d4846a75d59a3682a081a3b57
henry <henry@48f2787b-b4d3-4e6a-a033-8d54a5974186>
2011-09-11 00:27:29 +0000

    Inital add mail

--- www/application/controllers/admin.ctrlr.php
+++ www/application/controllers/admin.ctrlr.php
@@ -272,4 +272,5 @@ class AdminController
             return;
         }
     }
+
 }
--- www/application/views/admin/main.php
+++ www/application/views/admin/main.php
@@ -3,5 +3,5 @@
     <br/><br/>
     <a href="/admin/find_user">User pages</a><br/>
     <a href="/admin/pendingmenu_list">Pending menus [<?php echo $cnt_pending_menus; ?>]</a><br/>
-    
+    <a href="/mail/tester">Send mail</a><br/>
 </div>
