868eddde48adf8299ed2c8bd3e7b44950d4080c4
henry <henry@48f2787b-b4d3-4e6a-a033-8d54a5974186>
2011-09-24 06:04:27 +0000

    Remove reference to mylib.php

--- www/library/bootstrap.php
+++ www/library/bootstrap.php
@@ -2,5 +2,4 @@
 session_start();
 
 require_once (ROOT . DS . 'config' . DS . 'config.php');
-require_once (ROOT . DS . 'library' . DS . 'mylib.php');
 require_once (ROOT . DS . 'library' . DS . 'shared.php');
