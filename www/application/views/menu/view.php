5d0f5a83e8985c3a0070f748d99f9062e7b2c04b
henry <henry@48f2787b-b4d3-4e6a-a033-8d54a5974186>
2011-10-11 05:40:35 +0000

    Initial /menu/view/

--- www/application/views/menu/view.php
+++ www/application/views/menu/view.php
@@ -21,6 +21,43 @@ $params = array(
 extract($params, EXTR_SKIP);
 
 ?>
-<div class="pg">
+<div class="pg info">
+    <div class="name"><?php echo $info['name']; ?></div>
+    <div class="notes"><?php echo nl2br($info['notes']); ?></div>
+    <div class="address"><?php echo nl2br($info['address']); ?></div>
+    <div class="phone"><?php echo nl2br($info['numbers']); ?></div>
+    <div class="hours"><?php echo nl2br($info['hours']); ?></div>
+</div>
+<div class="pg links">
+<?php
+    foreach ($links as $link)
+    {
+        $label = $link['label'];
+        $url = $link['url'];
+
+        if (empty($label)) $label = $url;
+
+        echo<<<EOHTML
+            <div class="link"><a target="_blank" href="{$url}">{$label}</a></div>
+EOHTML;
+    }
+?>
+</div>
+<div class="pg menus">
+<?php foreach ($mdts as $mdt) { ?>
+    <div class="menu">
+        <div class="info">
+            <div class="name"><?php echo $mdt['name']; ?></div>
+            <div class="notes"><?php echo $mdt['notes']; ?></div>
+        </div>
+        <div class="items">
+        <?php foreach($mdt['items'] as $item) { ?>
+            <div class="item"><?php echo $item['item']; ?></div>
+            <div class="price"><?php echo $item['price']; ?></div>
+            <div class="notes"><?php echo $item['notes']; ?></div>
+        <?php } // foreach($mdt['items'] as $item) ?>
+        </div>
+    </div>
+<?php }// foreach ($mdts as $mdt) ?>
 </div>
 <div class="pg dbg"><pre><?php var_export($dbg); ?></pre></div>
