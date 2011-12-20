3cbaaa83f9a8954d78eca852f92ab8f02a7ce1cd
henry <henry@48f2787b-b4d3-4e6a-a033-8d54a5974186>
2011-12-20 05:01:42 +0000

    Fix caching for images and add etag header

--- www/application/views/images/generic_image.php
+++ www/application/views/images/generic_image.php
@@ -2,11 +2,7 @@
 // NOTE: image caching
 //       http://drupal.org/node/25977
 //       http://betterexplained.com/articles/how-to-optimize-your-site-with-http-caching/
-
-/*
-    From testing, 'If-Modified-Since' doesn't works 100%.
-    Maybe look into ETags
-*/
+//       http://rd2inc.com/blog/2005/03/making-dynamic-php-pages-cacheable/
 
 $img_src = $img_file['path'] . DS . $img_file['filename'];
 
@@ -16,27 +12,50 @@ if (!file_exists($img_src))
 }
 
 $request = apache_request_headers();
-$modified = filemtime($img_src);
 
-if (isset($request['If-Modified-Since']))
-{
-    //remove information after the semicolon and form a timestamp
-    $request_modified = explode(';', $request['If-Modified-Since']);
-    $request_modified = strtotime($request_modified[0]);
-
-    if ($modified <= $request_modified)
-    {
-        header('HTTP/1.1 304 Not Modified');
-        exit();
-    }
-}
+$referer = $request['Referer'];
 
 $mime = mime_content_type($img_src);
 
-$modified_date = gmdate('D, d M Y H:i:s', $modified);
+$stat = stat($img_src);
+$mtime = $stat['mtime'];
+$fsize = $stat['size'];
 
-header("Content-type: $mime");
-header("Last-Modified: {$modified_date} GMT");
+$etag = sprintf('"%x-%x-%x"', $stat['ino'], $stat['size'], $stat['mtime']);
+
+if (isset($request['If-Modified-Since']) && isset($request['If-None-Match']))
+{
+    //remove information after the semicolon and form a timestamp
+    $request_mtime = explode(';', $request['If-Modified-Since']);
+    $request_mtime = strtotime($request_mtime[0]);
+
+    $request_etag = $request['If-None-Match'];
+
+    if (($mtime <= $request_mtime) && ($etag === $request_etag))
+    {
+        //Util::logit("Cached on client side: '{$referer}'");
+        header('HTTP/1.1 304 Not Modified');
+        header("Content-Type: $mime");
+        header('Cache-Control: public');
+        header("ETag: {$etag}");
+        exit();
+    }
+    else
+    {
+        // shouldn't be here... but if it does, lets analyze the output
+        Util::logit("Cache miss: mtime('{$mtime}', '$request_mtime') etag('{$etag}', '{$request_etag}')");
+    }
+}
+
+//Util::logit("Not cached on client side: '{$referer}'");
+
+$modified_date = gmstrftime("%a, %d %b %Y %T %Z", $mtime);
+
+header("Content-Type: $mime");
+header("Content-Length: $fsize");
 header('Cache-Control: public');
+header('Expires: ');
+header("Last-Modified: {$modified_date}");
+header("ETag: {$etag}");
 
 readfile($img_src);
