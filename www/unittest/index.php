128b227c95d47cb92c4a9aa5139b589404e624bb
henry <henry@48f2787b-b4d3-4e6a-a033-8d54a5974186>
2011-12-31 21:44:18 +0000

    Organize unit tests

--- www/unittest/index.php
+++ www/unittest/index.php
@@ -1,9 +1,61 @@
 <?php
 
-$unit_tests[] = 'ut_login';
-
-foreach ($unit_tests as $ut)
+if (empty($_GET))
 {
-    $file = $ut.'.php';
-    require_once($file);
+    show_tests();
+}
+else
+{
+    $test = $_GET['test'];
+    exec_test($test);
+}
+
+function logit($obj, $file=null, $line=null)
+{
+    $from = "";
+    if (!empty($file) && !empty($line))
+        $from = "({$file}:{$line}): ";
+
+/*
+    if (is_string($obj))
+        error_log($from.$obj);
+    else
+        error_log($from.var_export($obj, true));
+*/
+
+    if (is_string($obj))
+        echo ($from.$obj);
+    else
+        echo ($from.var_export($obj, true));
+}
+
+function toPostStr($params)
+{
+    $fields_string = '';
+    foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+    rtrim($fields_string,'&');
+
+    return $fields_string;
+}
+
+function show_tests()
+{
+    $unit_tests[] = 'login';
+
+    foreach ($unit_tests as $ut)
+    {
+        echo<<<EOHTML
+            <a href="/unittest/index.php?test={$ut}">{$ut}</a>
+            <br/>
+EOHTML;
+    }
+}
+
+function exec_test($test)
+{
+    $ut_file = "ut_{$test}.php";
+
+    echo '<pre>';
+    require_once($ut_file);
+    echo '<pre>';
 }
--- www/unittest/ut_login.php
+++ www/unittest/ut_login.php
@@ -1,4 +1,5 @@
 <?php
+/*
 // http://davidwalsh.name/execute-http-post-php-curl
 $url = 'http://www.gogomenu.com/login/main&goto=home/main';
 
@@ -34,3 +35,62 @@ error_log(var_export($info, true));
 
 //close connection
 curl_close($ch);
+
+*/
+
+$ch = false;
+
+cinit();
+cparams();
+cwork();
+cdone();
+
+function cinit()
+{
+    global $ch;
+
+    $url = 'http://www.gogomenu.com/login/main';
+
+    //open connection
+    $ch = curl_init();
+
+    // setup curl
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+}
+
+function cparams()
+{
+    global $ch;
+
+    $fields = array
+    (
+        'lu' => 'admin',
+        'lp' => 'henry',
+    );
+
+    $post = toPostStr($fields);
+
+    curl_setopt($ch, CURLOPT_POST, count($fields));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+}
+
+function cdone()
+{
+    global $ch;
+
+    curl_close($ch);
+}
+
+function cwork()
+{
+    global $ch;
+
+    //execute post
+    $result = curl_exec($ch);
+
+    $info = curl_getinfo($ch);
+
+    logit($info);
+
+}
