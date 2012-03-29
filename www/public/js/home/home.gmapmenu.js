b57c259e34ea03fa8ec8321c6f4212d7c5c6f042
henry <hleong25@gmail.com>
2012-03-28 22:33:47 -0700

    Polish the js for gmap menu

--- www/public/js/home/home.gmapmenu.js
+++ www/public/js/home/home.gmapmenu.js
@@ -7,52 +7,56 @@ return {
 };
 
 var $map;
+var $bounds;
+var $infowindow;
 
 function init()
 {
     var centerlatlng = new google.maps.LatLng(33.735911, -117.787177);
     var myOptions = {
         center: centerlatlng,
-        zoom: 10,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
-    var map_canvas = $('#map_canvas')[0]; // must NOT be a jquery obj
+    var map_canvas = document.getElementById('map_canvas');
 
     $map = new google.maps.Map(map_canvas, myOptions);
-
-    var infowindow = new google.maps.InfoWindow();
-    var bounds = new google.maps.LatLngBounds();
+    $bounds = new google.maps.LatLngBounds();
+    $infowindow = new google.maps.InfoWindow();
 
     for (var idx in $menus)
     {
         var menu = $menus[idx];
 
-        var lat = menu['latitude'];
-        var lng = menu['longitude'];
-        var latlng = new google.maps.LatLng(lat, lng);
-
-        var marker = new google.maps.Marker({
-            map: $map,
-            position: latlng
-        });
-
-        bounds.extend(latlng);
-
-        google.maps.event.addListener(marker, 'click', (function(marker, menu) {
-            return function (){
-                var name = menu['name'];
-                var addy = menu['address'];
-                var link = menu['link'];
-
-                var info = '<div>'+link+'<br/><span>'+addy+'</span></div>';
-                infowindow.setContent(info);
-                infowindow.open($map, marker);
-            }
-        })(marker, menu));
+        addMarker(menu);
     }
 
-    $map.fitBounds(bounds);
+    $map.fitBounds($bounds);
+}
+
+function addMarker(menu)
+{
+    var lat = menu['latitude'];
+    var lng = menu['longitude'];
+    var latlng = new google.maps.LatLng(lat, lng);
+
+    var marker = new google.maps.Marker({
+        map: $map,
+        position: latlng
+    });
+
+    $bounds.extend(latlng);
+
+    google.maps.event.addListener(marker, 'click', function() {
+        var name = menu['name'];
+        var addy = menu['address'];
+        var link = menu['link'];
+
+        var info = '<div>'+link+'<br/><span>'+addy+'</span></div>';
+
+        $infowindow.setContent(info);
+        $infowindow.open($map, marker);
+    });
 }
 
 })();
