e00caa9ec2b80f894c89912bbd3b6597ab29ad8b
henry <hleong25@gmail.com>
2012-09-18 00:14:46 -0700

    Make "Forgot" the same line as "Username"

--- www/application/views/login/main.php
+++ www/application/views/login/main.php
@@ -8,15 +8,14 @@ extract($params, EXTR_SKIP);
 ?>
 <div class="pg main">
 <form id="login" method="post" action="/<?=$myurl?><?=(!empty($goto_url) ? '&'.$goto_url : '')?>">
-    <a class="forgot clearfix" href="/login/forgot">Forgot?</a>
-
     <label>
-        <span class="hint">Username</span>
+        <a class="forgot clearfix" href="/login/forgot">Forgot?</a>
+        <p class="hint">Username</p>
         <input id="username" class="watermark txt" type="text" name="lu" placeholder="Username" value="<?php echo $username; ?>" />
     </label>
 
     <label>
-        <span class="hint">Password</span>
+        <p class="hint">Password<p>
         <input class="watermark txt" type="password" name="lp" placeholder="Password" />
     </label>
 
