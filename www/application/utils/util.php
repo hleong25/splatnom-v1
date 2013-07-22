fe18de6ed1ad27ed2ccd9ad36a25502737d64c3f
henry <hleong25@gmail.com>
2013-07-21 23:25:23 -0700

    Unicode string split is bad. Need alternative. For now, use string split

--- www/application/utils/util.php
+++ www/application/utils/util.php
@@ -269,15 +269,20 @@ class Util
     static function str_split_unicode($str, $l = 0)
     {
         // http://www.php.net/manual/en/function.str-split.php#107658
-        if ($l > 0) {
-            $ret = array();
-            $len = mb_strlen($str, 'UTF-8');
-            for ($i = 0; $i < $len; $i += $l) {
-                $ret[] = mb_substr($str, $i, $l, 'UTF-8');
-            }
-            return $ret;
-        }
-        return preg_split("//u", $str, -1, PREG_SPLIT_NO_EMPTY);
+        //if ($l > 0) {
+        //    $ret = array();
+        //    $len = mb_strlen($str, 'UTF-8');
+        //    for ($i = 0; $i < $len; $i += $l) {
+        //        $ret[] = mb_substr($str, $i, $l, 'UTF-8');
+        //    }
+        //    return $ret;
+        //}
+        //return preg_split("//u", $str, -1, PREG_SPLIT_NO_EMPTY);
+
+        if ($l > 0)
+            return str_split($str, $l);
+
+        return array($str);
     }
 
     static function strEndsWith($haystack, $needle)
